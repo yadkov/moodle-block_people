1de6b3f59e675aa862d917acfcd7ecbdae32ab87
Georgi Yadkov <gjadkov@gmail.com>
2019-01-29 10:11:16 +0200

    formatted the list of users and delete the link for the participants section

--- block_people.php
+++ block_people.php
@@ -85,7 +85,6 @@ class block_people extends block_base {
         foreach ($teachers as $id => $tr) {
             if (count($tr) > 0) {
                 // Write heading and open new list
-//                $this->content->text .= html_writer::tag('h3', $rolenames[$id]);
                 $this->content->text .= html_writer::start_tag('ul');
 
                 // Do for every teacher with this role
@@ -110,19 +109,7 @@ class block_people extends block_base {
                     $this->content->text .= html_writer::start_tag('div', array('class' => 'name'));
                     $this->content->text .= fullname($t);
                     $this->content->text .= html_writer::end_tag('div');
-                    $this->content->text .= html_writer::start_tag('div', array('class' => 'icons'));
-                        if (has_capability('moodle/user:viewdetails', $currentcontext)) {
-                            $this->content->text .= html_writer::start_tag('a', array('href' => new moodle_url('/user/view.php', array('id' => $t->id, 'course' => $COURSE->id)), 'title' => get_string('viewprofile', 'core')));
-                            $this->content->text .= html_writer::empty_tag('img', array('src' => $OUTPUT->pix_url('i/user'), 'class' => 'icon', 'alt' => get_string('viewprofile', 'core')));
-                            $this->content->text .= html_writer::end_tag('a');
-                        }
-
-                        if ($CFG->messaging && has_capability('moodle/site:sendmessage', $currentcontext) && $t->id != $USER->id) {
-                            $this->content->text .= html_writer::start_tag('a', array('href' => new moodle_url('/message/index.php', array('id' => $t->id)), 'title' => get_string('sendmessageto', 'core_message', fullname($t))));
-                                $this->content->text .= html_writer::empty_tag('img', array('src' => $OUTPUT->pix_url('t/email'), 'class' => 'icon', 'alt' => get_string('sendmessageto', 'core_message', fullname($t))));
-                            $this->content->text .= html_writer::end_tag('a');
-                        }
-                    $this->content->text .= html_writer::end_tag('div');
+                    $this->content->text .= html_writer::tag('div', $rolenames[$id]);
 
                     $this->content->text .= html_writer::end_tag('li');
                 }
@@ -135,24 +122,6 @@ class block_people extends block_base {
         // End teachers list
         $this->content->text .= html_writer::end_tag('div');
 
-        // Output participant list
-        $this->content->text .= html_writer::start_tag('div', array('class' => 'participants'));
-        $this->content->text .= html_writer::tag('h3', get_string('participants'));
-
-        // Only if user is allow to see participants list
-        if (has_capability('moodle/course:viewparticipants', $currentcontext)) {
-            $this->content->text .= html_writer::start_tag('a', array('href' => new moodle_url('/user/index.php', array('contextid' => $currentcontext->id)), 'title' => get_string('participants')));
-            $this->content->text .= html_writer::empty_tag('img', array('src' => $OUTPUT->pix_url('i/users'), 'class' => 'icon', 'alt' => get_string('participants')));
-            $this->content->text .= get_string('participantslist', 'block_people');
-            $this->content->text .= html_writer::end_tag('a');
-        }
-        else {
-            $this->content->text .= html_writer::start_tag('span', array('class' => 'hint'));
-            $this->content->text .= get_string('noparticipantslist', 'block_people');
-            $this->content->text .= html_writer::end_tag('span');
-        }
-
-        $this->content->text .= html_writer::end_tag('div');
 
         return $this->content;
     }
